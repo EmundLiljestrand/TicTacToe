ed2daf384190867fc48a18f78029757a43fd48cc
Tivva34 <gemtime1991@gmail.com>
2025-01-23 15:50:21 +0100

    jahaja

--- script.js
+++ script.js
@@ -6,20 +6,24 @@
  */
 let oGameData = {}; //main objectet används under hela game loopen, allt som är relevant med spelet 
 
-window.addEventListener("load", () => {
+//#newGame- id, ifrån html rad 50
 
-    initGlobalObject(); //ger globala objectet ett "värde" i functionen
-    if (checkForGameOver() === 1) { //kollar om vilken spelare vinner
-        console.log("Spelare 1 vann");
-    } else if (checkForGameOver() === 2) {
-        console.log("Spelare 2 vann");
-    } else if (checkForGameOver() === 3) {
-        console.log("Oavgjort");
-    } else {
-        console.log("Spelet fortsätter");
-    }
-});
 
+
+//window.addEventListener("load", () => {
+//     initGlobalObject(); //ger globala objectet ett "värde" i functionen
+//     if (checkForGameOver() === 1) { //kollar om vilken spelare vinner
+//         console.log("Spelare 1 vann");
+//     } else if (checkForGameOver() === 2) {
+//         console.log("Spelare 2 vann");
+//     } else if (checkForGameOver() === 3) {
+//         console.log("Oavgjort");
+//     } else {
+//         console.log("Spelet fortsätter");
+//     }
+//});
+
+prepGame();
 
 
 /**
@@ -39,9 +43,11 @@ function initGlobalObject() {
     // kollar om spelare 1 vinner
     console.log(checkForGameOver()); //kollar om allt stämmer,
 
+
+
+
+
     /* oGameData.gameField = ['X', '', '', 'X', '', '', 'X', '', '']; */
-
-
     //oGameData.gameField = ['X', '', '', '', 'X', '', '', '', 'X'];
     //oGameData.gameField = ['', '', 'X', '', 'X', '', 'X', '', ''];
     //oGameData.gameField = ['X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O'];
@@ -56,7 +62,10 @@ function initGlobalObject() {
     oGameData.currentPlayer = "";
 
     //Nickname för spelare ett som tilldelas från ett formulärelement,
+    /* oGameData.nickNamePlayerOne = document.getElementById("#nick1"); */
     oGameData.nickNamePlayerOne = "";
+
+
 
     //Nickname för spelare två som tilldelas från ett formulärelement.
     oGameData.nickNamePlayerTwo = "";
@@ -78,6 +87,8 @@ function initGlobalObject() {
 
     //Referens till element för felmeddelanden
     oGameData.timeRef = document.querySelector("#errorMsg")
+
+
 }
 
 
@@ -160,18 +171,174 @@ function checkForDraw() {
 
 // Nedanstående funktioner väntar vi med!
 
-function prepGame() { }
+function prepGame() {
+    document.querySelector("#gameArea").classList.add('d-none');
+    document.querySelector("#newGame").addEventListener("click", initiateGame);
+}
 
-function validateForm() { }
+function validateForm() {
+    //hanterare/ kollar om spelarna har valt nick/färger
 
-function initiateGame() { }
 
-function executeMove(event) { }
+}
+
+function initiateGame() {
+    document.querySelector("#theForm").classList.add("d-none");
+    document.querySelector("#gameArea").classList.remove('d-none');
+    document.querySelector("#errorMsg").textContent = "";
+
+    oGameData.nickNamePlayerOne = document.querySelector('#nick1').value;
+    oGameData.nickNamePlayerTwo = document.querySelector('#nick2').value;
+    oGameData.colorPlayerOne = document.querySelector('#color1').value;
+    oGameData.colorPlayerTwo = document.querySelector('#color2').value;
+
+    const cells = document.querySelectorAll("td");
+    cells.forEach((cells) => {
+        cells.textContent = "";
+        cells.style.backgroundcolor = "#ffffff";
+    });
+
+    let playerChar = "";
+    let playerName = "";
+
+    const randomNmr = Math.random();
+
+    if (randomNmr < 0.5) {
+        playerChar = oGameData.playerOne;
+        playerName = oGameData.nickNamePlayerOne;
+        oGameData.currentPlayer = oGameData.playerOne;
+
+        //if random number > 0.5
+    } else {
+        playerChar = oGameData.playerTwo;
+        playerName = oGameData.nickNamePlayerTwo;
+        oGameData.currentPlayer = oGameData.playerTwo;
+    }
+    //if random number > 0.5
+
+
+    //Ändra texten i h1-elementet som ligger i div-elementet med klassen "jumbotron" till "Aktuell spelare är XXX",
+    // där ni ersätter XXX med namnet på den aktuella spelaren.
+
+    //"hämtar" h1 classen jumbotron ifrån htmlen
+    document.querySelector(".jumbotron").textContent = `Aktuell spelare är ${playerName}`;
+
+    document.querySelector("#gameArea").addEventListener("click", executeMove);
+    //tog bort execute(), man skall inte kalla på functionen; 
+
+    //Lägg till en klicklyssnare på tabellen som innehåller spelplanen.
+    // Vid klick skall funktionen "executeMove()" anropas.
+
+
+
+}
+
+
+
+function executeMove(event) {
+    const clickedCell = event.target;
+
+    // Kontrollera att klicket skedde på en cell
+    if (clickedCell.tagName !== "TD") return;
+
+    // Kontrollera att cellen är ledig
+    if (clickedCell.textContent !== "") return;
+
+    // Sätt spelets datafält till nuvarande spelare
+    clickedCell.getAttribute("data-id");
+    oGameData.gameField = oGameData.currentPlayer;
+
+    // Sätt cellens bakgrundsfärg och text till nuvarande spelare
+    const playerColor = oGameData.currentPlayer === "X" ? oGameData.playerOneColor : oGameData.playerTwoColor;
+    clickedCell.style.backgroundColor = playerColor;
+    clickedCell.textContent = oGameData.currentPlayer;
+
+    // Byt spelare
+    oGameData.currentPlayer = oGameData.currentPlayer === "X" ? "O" : "X";
+    const nextPlayerName = oGameData.currentPlayer === "X" ? oGameData.nickNamePlayerOne : oGameData.nickNamePlayerTwo;
+    document.querySelector(".jumbotron h1").textContent = `Aktuell spelare är ${playerName}`;
+
+    // Kontrollera om spelet är slut
+    const result = checkGameStatus();
+    if (result !== 0) {
+        gameOver(result);
+    }
+}
+
+// function executeMove(event) {
+//     // Kontrollera att klicket är på en tom cell
+//     if (event.target.tagName !== "TD" || event.target.textContent !== "") {
+//         return;
+//     }
+
+//     // Hämta data-id från cellen
+//     const dataid = event.target.getAttribute(dataid);
+//     if (!dataid) {
+//         console.error("Kunde inte hitta data-id på den klickade cellen.");
+//         return;
+//     }
+
+//     // Uppdatera gameField
+//     oGameData.gameField[dataid] = oGameData.currentPlayer;
+//     console.log("gameField efter uppdatering:", oGameData.gameField);
+
+//     // Uppdatera cellens innehåll och stil
+//     event.target.textContent = oGameData.currentPlayer;
+//     console.log(`Satte text till: ${oGameData.currentPlayer} för cell med data-id: ${dataid}`);
+
+//     if (oGameData.currentPlayer === oGameData.playerOne) {
+//         event.target.style.backgroundColor = oGameData.colorPlayerOne;
+//     } else {
+//         event.target.style.backgroundColor = oGameData.colorPlayerTwo;
+//     }
+
+//     // Kontrollera om spelet är över
+//     const gameStatus = checkForGameOver();
+//     if (gameStatus === 0) {
+//         changePlayer();
+//     } else {
+//         gameOver(gameStatus);
+//     }
+// }
+
+
+
+
+/* function executeMove(event) {
+    if (event.target.tagName !== "TD") {
+        return;
+    }
+
+    if (event.target.textContent !== "") {
+        return;
+    }
+
+    const dataid = event.target.getAttribute("dataid");
+    if (dataid === null) {
+        console.error("dataid is null");
+        return;
+    }
+
+    oGameData.gameField[dataid] = oGameData.currentPlayer;
+    event.target.textContent = oGameData.currentPlayer;
+
+    if (oGameData.currentPlayer === oGameData.playerOne) {
+        event.target.style.backgroundColor = oGameData.colorPlayerOne;
+    } else {
+        event.target.style.backgroundColor = oGameData.colorPlayerTwo;
+    }
+
+    if (checkForGameOver() === 0) {
+        changePlayer();
+    } else {
+        gameOver(checkForGameOver());
+    }
+}
+ */
+
 
 function changePlayer() { }
 
 function timer() { }
 
 function gameOver(result) { }
-
-
